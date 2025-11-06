3ac0c6223a7336b63b9650b3be5457fa6e84b59a
Nikita bhoi <114177037+nikitasanjubhoi@users.noreply.github.com>
2025-11-06 19:49:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,11 +9,15 @@ function App() {
 
   const search = async (e) => {
     if (e.key === "Enter") {
-      const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}`
-      );
-      setWeather(response.data);
-      setQuery("");
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}`
+        );
+        setWeather(response.data);
+        setQuery("");
+      } catch (error) {
+        alert("City not found or API error!");
+      }
     }
   };
 
@@ -27,8 +31,9 @@ function App() {
         placeholder="Enter a city"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        onKeyPress={search}
+        onKeyDown={search}
       />
+
       {weather && (
         <div className="weather">
           <div className="city">{weather.name}</div>
@@ -38,7 +43,7 @@ function App() {
           <div className="description">{weather.weather[0].description}</div>
           <div className="icon">
             <img
-              src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+              src={`https://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
               alt={weather.weather[0].description}
             />
           </div>
